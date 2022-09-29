637202f213ca7d0153bf3874647e0735c3fcba3d
Sara Choi <sarachoi10@hotmail.com>
2022-09-29 07:48:21 +0000

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+for (let i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+let result = 0;
+for (let m = 0; m < 10; m += 1) {
+  console.log(result);
+  result += 2;
+}
+
+for (let x = 100; x >= 0; x--) {
+  console.log('Time till explosion ' + x + '!');
+}
